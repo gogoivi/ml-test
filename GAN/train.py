Basic gan model made - need to fix loss and training loop so that it backprops the descriminator k times before updating G

GAN/train.py
@@ -1,5 +1,6 @@
+# Compied up to dataloader from VAE since it isnt anything different
 import torch
-from model import VAE, vae_loss,generate_image
+from model import GAN_loss,Descriminator,Generator
 import torchvision
 from torchvision.transforms import ToTensor
 from tqdm import tqdm
@@ -17,6 +18,7 @@ def save_model(model: torch.nn.Module, target_dir: str, model_name: str):
 height=28
 width=28
 epochs=50
+z_size=25
 
 # Copied from some of my previously written code
 if __name__=="__main__":
@@ -38,27 +40,35 @@ if __name__=="__main__":
     batch=512
     train_dataloader =torch.utils.data.DataLoader(train_data,batch_size=batch)
     test_dataloader=torch.utils.data.DataLoader(test_data,batch_size=batch)
-    vae=VAE(z_size=25,height=height,width=width,color=False)
-    vae.to(device)
+    descriminator=Descriminator(height=height,width=width,color=False)
+    descriminator.to(device)
+    generator=Generator(z_size=z_size,height=height,width=width,color=False)
+    generator.to(device)
 
-    optimizer=torch.optim.Adamax(params=vae.parameters(),lr=0.01)
+    de_optimizer=torch.optim.Adamax(params=descriminator.parameters(),lr=0.01)
+    ge_optimizer=torch.optim.Adamax(params=generator.parameters(),lr=0.01)
 
     for i in tqdm(range(epochs)):
-        vae.train()
-        loss_sum=0
+        generator.train()
+        descriminator.train()
+        Des_loss_sum=0
         for batch, (X,_) in enumerate(train_dataloader):
             X=X.to(device)
-            x_pred,mu,log_var=vae(X)
-            loss=vae_loss(x=X,x_reconstructed=x_pred,mu=mu,log_var=log_var)
-            loss_sum+=loss.item()
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+            current_batch_size = X.shape[0]
+            z_tensor = torch.normal(mean=0.0, std=1.0, size=(current_batch_size, z_size))
+            z_tensor=z_tensor.to(device)
+            ml_guess=descriminator(generator(z_tensor))
+            real_guess=descriminator(X)
+            Gen_loss,Des_loss=GAN_loss(ml_guess=ml_guess,real_guess=real_guess)
+            Des_loss_sum+=Des_loss.item()
+            de_optimizer.zero_grad()
+            Des_loss.backward()
+            de_optimizer.step()
+
+        ge_optimizer.zero_grad()
+        Gen_loss.backward()
+        ge_optimizer.step()
+        
         if i%5==0:
-            print(loss_sum/len(train_dataloader))
-
-    # save_model(model=vae,target_dir="VAE_models",model_name="MNIST_Z25_E50")
-
-    for i in range(3):
-        generate_image(vae,device)
-    
+            print(Des_loss_sum/len(train_dataloader))
+            print(Gen_loss.item()/len(train_dataloader))